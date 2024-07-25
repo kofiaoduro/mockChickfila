5320cba443529bb3f7117992211da9fbe6dcdc4a
Kofi Oduro <kofia.oduro@gmail.com>
2024-07-25 13:03:51 -0400

    Set up a cart model

---
+++ models/cart.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Product = require('../models/product')
+const Schema = mongoose.Schema
+
+main().catch(err => console.log(err));
+
+async function main() {
+
+        try{
+            await mongoose.connect('mongodb://127.0.0.1:27017/chickfilaApp');
+            console.log('database connected')
+        }
+        catch(e){
+            console.log(e)
+        }
+  
+}
+
+cartSchema = new Schema({
+    prducts: [{type: Schema.Types.ObjectId, ref: 'Product'}]
+})
+
+
+const Cart = mongoose.model('Cart', cartSchema)
+
+
+module.exports = Cart
